b801b80839595c99ca1445acae0fc00059c02a89
Narges Sadeghi <narges.sadeghi73@gmail.com>
2024-01-08 14:25:52 +0330

    store theme using hydrated bloc

--- lib/blocs/switch_bloc/switch_bloc.dart
+++ lib/blocs/switch_bloc/switch_bloc.dart
@@ -7,7 +7,7 @@ part 'switch_event.dart';
 part 'switch_state.dart';
 
 
-class SwitchBloc extends Bloc<SwitchEvent, SwitchState> {
+class SwitchBloc extends HydratedBloc<SwitchEvent, SwitchState> {
   SwitchBloc() : super(const SwitchInitial(switchValue: false)) {
     on<SwitchOnEvent>((event, emit) {
       emit(const SwitchState(switchValue: true));
@@ -15,5 +15,15 @@ class SwitchBloc extends Bloc<SwitchEvent, SwitchState> {
     on<SwitchOffEvent>((event, emit) {
       emit(const SwitchState(switchValue: false));
     });
+  }
+
+  @override
+  SwitchState? fromJson(Map<String, dynamic> json) {
+    return SwitchState.fromMap(json);
+  }
+
+  @override
+  Map<String, dynamic>? toJson(SwitchState state) {
+    return state.toMap();
   }
 }
